7b3028b28eb1f2ca5d9f31dac74089f82cc47505
MMate03 <matemihaly00@gmail.com>
2025-09-27 11:21:04 +0300

    init

---
+++ src/main/java/com/example/szamlakezelo/SzamlakezeloApplication.java
@@ -0,0 +1,13 @@
+package com.example.szamlakezelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzamlakezeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzamlakezeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/szamlakezelo/SzamlakezeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szamlakezelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzamlakezeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
