e69bd249a300b3a14acbd199357dc30a4f67c2ee
MMate03 <matemihaly00@gmail.com>
2025-09-27 11:56:27 +0300

    add: User model

---
+++ src/main/java/com/example/szamlakezelo/model/Role.java
@@ -0,0 +1,7 @@
+package com.example.szamlakezelo.model;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_ACCOUNTANT,
+    ROLE_USER
+}
---
+++ src/main/java/com/example/szamlakezelo/model/User.java
@@ -0,0 +1,30 @@
+package com.example.szamlakezelo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDate loginDate;
+
+    @Column(nullable = false)
+    private Role role;
+}
