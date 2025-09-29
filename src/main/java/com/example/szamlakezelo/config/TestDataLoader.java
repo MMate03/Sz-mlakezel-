a5d6f01ff917143312fa07ba15c97a65e8cf8dd3
MMate03 <matemihaly00@gmail.com>
2025-09-29 20:38:40 +0300

    add: user generation

--- src/main/java/com/example/szamlakezelo/config/TestDataLoader.java
+++ src/main/java/com/example/szamlakezelo/config/TestDataLoader.java
@@ -1,24 +1,56 @@
 package com.example.szamlakezelo.config;
 
 import com.example.szamlakezelo.model.Invoice;
+import com.example.szamlakezelo.model.Role_enum;
+import com.example.szamlakezelo.model.User;
 import com.example.szamlakezelo.repo.InvoiceRepository;
+import com.example.szamlakezelo.repo.RoleRepository;
+import com.example.szamlakezelo.repo.UserRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 @Component
 public class TestDataLoader implements CommandLineRunner {
 
     private final InvoiceRepository invoiceRepository;
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public TestDataLoader(InvoiceRepository invoiceRepository) {
+
+    public TestDataLoader(InvoiceRepository invoiceRepository,
+                          RoleRepository roleRepository,
+                          UserRepository userRepository,
+                          PasswordEncoder passwordEncoder) {
         this.invoiceRepository = invoiceRepository;
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args) {
+
+        String[] roles = {"ROLE_ADMIN", "ROLE_ACCOUNTANT", "ROLE_USER"};
+
+        for (Role_enum roleEnum : Role_enum.values()) {
+            String username = roleEnum.name().toLowerCase();
+            if (!userRepository.existsByUsername(username)) {
+                User user = new User();
+                user.setUsername(username);
+                user.setName(username);
+                user.setPassword(passwordEncoder.encode("test123"));
+                user.setRoles(Set.of(roleRepository.findByName(roleEnum)));
+                userRepository.save(user);
+            }
+        }
+
         if (invoiceRepository.count() == 0) {
             IntStream.rangeClosed(1, 30).forEach(i -> {
                 Invoice invoice = new Invoice();
